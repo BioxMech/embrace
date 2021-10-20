5bb89b6c9bbe8938daf41470a4a0857aff124e5e
huiqiii <hqphua21@gmail.com>
2021-10-20 19:27:01 +0800

    reminder page added
    need help with alignment

--- src/page/reminder/reminder.page.jsx
+++ src/page/reminder/reminder.page.jsx
@@ -4,44 +4,104 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
 
-// import './donate.styles.scss';
+import './reminder.styles.scss';
 import Clock from '../../asset/images/clock.png';
 
 function Reminder() {
+    const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
     return (
         <Box  my={2} py={8}>
           <Container maxWidth="md">
             <Grid container>
-              <Grid item xs={12}>
+            <Grid item xs={12} style={{ display:'flex', justifyContent:'center' }}>
                 <Typography variant="h3" style={{ fontWeight: 800 }} align="center" color="text.primary" gutterBottom>
                 Priyanka’s Reminders
                 </Typography>
-                <Grid item xs={12} >
-                <Box mb={3} style={{ textAlign: 'center' }}>
-                <img src={Clock} alt="..." className="clock-img" />
+                <Box mt={1} ml={2} style={{ textAlign: 'center' }}>
+                    <img src={Clock} alt="..." className="clock-img" />
                 </Box>
-                </Grid>
-                <Typography style={{color:"#ff4081", fontWeight: 800}} variant="h4" align="center" gutterBottom>
-                  We Need <i>Your</i> Help
+            </Grid>
+            <Grid container spacing={5}>
+            <Grid item xs={12}>
+                <Typography style={{color:"#9867C5"}} variant="h4" align="center" gutterBottom>
+                <b>PERIOD & FERTILITY</b>
                 </Typography>
-                <Box mt={5} mb={5}>
-                  <Typography variant="h6" align="center" color="text.primary" gutterBottom style={{ fontWeight: 400 }}>
-                    YOU can help us change the lives of 23 million girls - who drop out of school due to lack of menstrual hygiene and awareness. With as little as $1 you can prevent a girl in India from dropping out of school and change her life.
-                  </Typography>
-                </Box>
-                <Box mb={5}>
-                  <Typography variant="body1" align="center" color="text.primary" gutterBottom style={{ fontWeight: 400 }}>
-                    Come join us on this journey of normalizing menstruation in India. Your gift will go a long way in empowering women all across Telangana and beyond in the future.
-                  </Typography>
-                  <Typography variant="body1" align="center" color="text.primary" gutterBottom style={{ fontWeight: 400 }}>
-                    Your gift will go a long way in empowering women all across Telangana and beyond in the future.
-                  </Typography>
-                </Box>
-              </Grid>
-              </Grid>
+                
+                <Grid item xs={12} style={{ display:'flex', justifyContent:'center' }}>
+                    <Typography variant="h6" align="center" color="text.primary" gutterBottom style={{ fontWeight: 400 }}>
+                    Period is coming
+                    </Typography>
+                    <Box ml={25} style={{ textAlign: 'center' }}>
+                        <Switch {...label} defaultChecked style={{color:"#ff4081"}}/>
+                    {/* <Button size="small" fullWidth variant="contained" style={{ backgroundColor: "#ff4081"}} >ON</Button> */}
+                    </Box>
+                </Grid>
+                <Grid item xs={12} style={{ display:'flex', justifyContent:'center' }}>
+                    <Typography variant="h6" align="center" color="text.primary" gutterBottom style={{ fontWeight: 400 }}>
+                    Fertility is coming
+                    </Typography>
+                    <Box ml={25} style={{ textAlign: 'center' }}>
+                        <Switch {...label} defaultChecked style={{color:"#ff4081"}}/>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} style={{ display:'flex', justifyContent:'center' }}>
+                    <Typography variant="h6" align="center" color="text.primary" gutterBottom style={{ fontWeight: 400 }}>
+                    Ovulation day
+                    </Typography>
+                    <Box ml={25} style={{ textAlign: 'center' }}>
+                        <Switch {...label} defaultChecked style={{color:"#ff4081"}}/>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} style={{ display:'flex', justifyContent:'center' }}>
+                    <Typography variant="h6" align="center" color="text.primary" gutterBottom style={{ fontWeight: 400 }}>
+                    Input your period
+                    </Typography>
+                    <Box ml={25} style={{ textAlign: 'center' }}>
+                        <Switch {...label} defaultChecked style={{color:"#ff4081"}}/>
+                    </Box>
+                </Grid>
+            </Grid>
+            </Grid>
+
+            <Grid container spacing={5}>
+            <Grid item xs={12}>
+                <Typography style={{color:"#9867C5"}} variant="h4" align="center" gutterBottom>
+                <b>LIFESTYLE</b>
+                </Typography>
+                
+                <Grid item xs={12} style={{ display:'flex', justifyContent:'center' }}>
+                    <Typography variant="h6" align="center" color="text.primary" gutterBottom style={{ fontWeight: 400 }}>
+                    Meditation reminder
+                    </Typography>
+                    <Box ml={25} style={{ textAlign: 'center' }}>
+                        <Switch {...label} defaultChecked style={{color:"#ff4081"}}/>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} style={{ display:'flex', justifyContent:'center' }}>
+                    <Typography variant="h6" align="center" color="text.primary" gutterBottom style={{ fontWeight: 400 }}>
+                    Sleep early reminder
+                    </Typography>
+                    <Box ml={25} style={{ textAlign: 'center' }}>
+                        <Switch {...label} defaultChecked style={{color:"#ff4081"}}/>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} style={{ display:'flex', justifyContent:'center' }}>
+                    <Typography variant="h6" align="center" color="text.primary" gutterBottom style={{ fontWeight: 400 }}>
+                    Drink water reminder
+                    </Typography>
+                    <Box ml={25} style={{ textAlign: 'center' }}>
+                        <Switch {...label} defaultChecked style={{color:"#ff4081"}}/>
+                    </Box>
+                </Grid>
+            </Grid>
+            </Grid>
+
+        </Grid>
         </Container>
-        </Box>
+    </Box>
 
 )
 }
